4ff4629b3ba5e5101f3ad57fa01db5dc414cee87
khalfi aymen <khalfi.aymen2021@gmail.com>
2024-03-28 22:14:51 +0100

    Deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react"
-import ReactDOM from "react-dom/client" // Import from "react-dom" instead of "react-dom/client"
+import ReactDOM from "react-dom/client" 
 import App from "./App.jsx"
 import "./index.css"
 import { BrowserRouter } from "react-router-dom"
-import { AuthContextProvider } from "./context/AuthContext.jsx"
-import { SocketContextProvider } from "./context/SocketContext.jsx"
+import { AuthContextProvider } from "./context/AuthContext"
+import { SocketContextProvider } from "./context/SocketContext"
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
