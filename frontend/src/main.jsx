bc654e321e80634bac3443823168f5fc1a0757c7
khalfi aymen <khalfi.aymen2021@gmail.com>
2024-03-28 22:16:59 +0100

    Deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,19 @@
 import React from "react"
-import ReactDOM from "react-dom/client" 
+import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 import "./index.css"
 import { BrowserRouter } from "react-router-dom"
-import { AuthContextProvider } from "./context/AuthContext"
-import { SocketContextProvider } from "./context/SocketContext"
-
+import { AuthContextProvider } from "./context/AuthContext.jsx"
+import { SocketContextProvider } from "./context/SocketContext.jsx"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <AuthContextProvider>
-      <SocketContextProvider>
-        <App />
-      </SocketContextProvider>
-    </AuthContextProvider>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <SocketContextProvider>
+          <App />
+        </SocketContextProvider>
+      </AuthContextProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 )
