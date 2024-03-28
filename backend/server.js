1d5d33105e20e17ffaadc1b7ea3e2610cc95a568
khalfi aymen <khalfi.aymen2021@gmail.com>
2024-03-28 22:08:16 +0100

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,20 @@ import cors from "cors"
 import helmet from "helmet"
 import morgan from "morgan"
 import cookieParser from "cookie-parser"
+import path from "path"
 
 import connectToMongoDB from "./dataBase/connectToMongoDb.js"
 import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js" 
+import messageRoutes from "./routes/message.route.js"
 import userRoutes from "./routes/user.route.js"
 import { app, server } from "./socket/socket.js"
 
 dotenv.config()
 const PORT = process.env.PORT || 5000
 
-app.use(cookieParser()) 
+const __dirname = path.resolve()
+
+app.use(cookieParser())
 dotenv.config()
 app.use(express.json())
 app.use(helmet())
@@ -25,13 +28,15 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
 
-app.get("/", (req, res) => {
-  res.send("Server Running")
-})
-
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/messages", messageRoutes)
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 server.listen(PORT, () => {
   connectToMongoDB()
